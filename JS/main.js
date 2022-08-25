98514a3f2f552789c74301924baf0e2d3be05197
Jose Luis Pintado <joseluispintado666@gmail.com>
2022-08-25 12:01:37 -0300

    primeras pruebas del clima

--- JS/main.js
+++ JS/main.js
@@ -46,26 +46,27 @@ boton.addEventListener("click", onClick); // cuando se captura el clic del boton
 
 
 
-
-// async function getIp() {
-//     try { 
-//       /*  
-//       let response = await fetch("https://api.ipify.org/?format=json"); 
-//       let ipResponse = await response.json(); 
-//       console.log(ipResponse);
+/* probando con clima */
+async function getIp() {
+    try { 
+      /*  
+      let response = await fetch("https://api.ipify.org/?format=json"); 
+      let ipResponse = await response.json(); 
+      console.log(ipResponse);
   
-//       let responseLocation = await fetch("http://ip-api.com/json/" + ipResponse.ip);
-//       let locationResponse = await responseLocation.json();
-//       console.log(locationResponse);
-//       */
+      let responseLocation = await fetch("http://ip-api.com/json/" + ipResponse.ip);
+      let locationResponse = await responseLocation.json();
+      console.log(locationResponse);
+      */
 
-//       let responseClima = await fetch("http://api.openweathermap.org/data/2.5/forecast?lat=-24.183241513504793&lon=-65.33131935975862&appid=67823e81057e9209280832cf8105fa65");
-    
-//     } catch { 
-//       console.log("Algo paso, no se pudo resolver...");
-//     }
-//   }
-//   getIp();
+        let responseClima = await fetch("http://api.openweathermap.org/data/2.5/forecast?lat=-24.183241513504793&lon=-65.33131935975862&appid=67823e81057e9209280832cf8105fa65");
+        let locationResponse = await responseClima.json();
+        console.log(locationResponse);
+    } catch { 
+      console.log("Algo paso, no se pudo resolver...");
+    }
+  }
+  getIp();
 
   //-24.183241513504793, -65.33131935975862
 
