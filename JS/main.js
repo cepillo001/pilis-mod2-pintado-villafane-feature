fe870f9c0f5db07be23917725c2e8715c27d3d8a
Jose Luis Pintado <joseluispintado666@gmail.com>
2022-08-26 11:03:52 -0300

    descripcion en clima y retoques css

--- JS/main.js
+++ JS/main.js
@@ -54,13 +54,14 @@ async function getIp() {
         let title = document.getElementById("climaContentTitle");
         let icon = document.getElementById("climaContentIcon");
         let temp = document.getElementById("climaContentTemp");
-        // innerHTL inyecta el codigo en la pagina
+        let description = document.getElementById("climaContentDescription");
+        // innerHTML inyecta el codigo en la pagina
         title.innerHTML = locationResponse.name;
-
         // para renderizar el icon, usamos la url http://openweathermap.org/img/wn/10d@2x.png, donde "10d" es el icon que recibimos
         icon.innerHTML = "<img src='http://openweathermap.org/img/wn/" + locationResponse.weather[0].icon + "@2x.png'>";
         // con Math.roud redondeo el clima a entero.
         temp.innerHTML = Math.round(locationResponse.main.temp) + " °C";
+        description.innerHTML = locationResponse.weather[0].description;
 
     } catch { 
       console.log("Algo paso, no se pudo resolver...");
